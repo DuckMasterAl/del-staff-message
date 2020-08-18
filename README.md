8bca1a6d441cba0dfdd042c6a22984793152c852
DuckMasterAl <43557963+DuckMasterAl@users.noreply.github.com>
2020-08-18 17:27:54 -0400

    It's done :D

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # Discord Extreme List Staff Messaging Service
-:construction: This is currently being created! Please check back soon!! :construction:
+A Service that lets you make professional messages for the Approval Queue!
+
+## What does this do?
+This system just let's you list errors with a bot, and copies the message for you to send back to the owner! To end typing issues, just hit return/enter!
+
+## Setup
+*Note: This doesn't tell you how to install Python and stuff. I'm also not sure what Python Versions this works on, I am using Python 3.8.0*
+1. Download the [del_msg.py](https://github.com/DuckMasterAl/del-staff-message/blob/master/del_msg.py) File
+2. Create a file with the text in [example_data.json](https://github.com/DuckMasterAl/del-staff-message/blob/master/example_data.json) called `del_msg_data.json` (This can be changed at the second line of [del_msg.py](https://github.com/DuckMasterAl/del-staff-message/blob/master/del_msg.py#L2))
+3. Install pyperclip by typing `pip3 install pyperclip` into Command Prompt/Terminal
+4. Run [del_msg.py](https://github.com/DuckMasterAl/del-staff-message/blob/master/del_msg.py) and use the DEL Staff Messaging Service!
+
+### Disclaimers
+We are not affiliated with Discord Extreme List. "Discord Extreme List", "discordextremelist.xyz" (case-insensitive) are trademarks of Cairo Mitchell-Acason.
--- del_msg.py
+++ del_msg.py
@@ -1,15 +1,106 @@
-import pyperclip
-a = 0
-num = 1
-msg =['Hello! While reviewing your bot, I found the following issues:']
-while a == 0:
-        issue = input('List an Issue with the Bot. If you are done return nothing.\n')
-    if issue == '':
-        a = 1
+import pyperclip, json
+datafile = '/Users/duckmasteral/Documents/GitHub/del-staff-message/del_msg_data.json'# You may need to change the path to the data file.
+
+start = input('Welcome to the DEL Staff Messaging System!\n[1] Review a New Bot\n[2] Re-review a bot\n[3] Remove an Approved/Denied Bot\nPlease type the number that corresponds to the action you want to do: ')
+if start == '1':
+    botid = input("\nPlease enter the bot's username for re-reviewal purposes.\nIf you would like to skip this step, don't send anything: ")
+    if botid != '':
+        bot_id = botid
+        ownerid = input("\nPlease enter the owners's user id for re-reviewal purposes.\nIf you would like to skip this step, don't send anything: ")
     else:
-        msg.append(f"> {num}. {issue}")
+        bot_id = 'Unknown'
+    if botid != '':
+        with open(datafile) as json_file:
+            data = json.load(json_file)
+        if ownerid == '':
+            ownerid = ''
+            owner_id = 'Unknown'
+        else:
+            ownerid = int(ownerid)
+            owner_id = ownerid
+        temp = {"owner": ownerid, "bot": botid}
+        data['bots'].append(temp)
+        with open(datafile, 'w') as f:
+            json.dump(data, f, indent=4)
+    a = 0
+    num = 1
+    msg =['Hello! While reviewing your bot, I found the following issues:']
+    print('Start typing errors below!')
+    while a == 0:
+        try:
+            issue = input('\n- ')
+        except Traceback:
+            pyperclip.copy(msg)
+            print('Copied all the Bot\'s Issues to your Clipboard! Next time, you can paste them and get started again!')
+        if issue == '':
+            a = 1
+        else:
+            if issue.endswith('.') == False:
+                issue = f'{issue}.'
+            msg.append(f"> {num}. {issue}")
+            num += 1
+    msg.append("Please ping me __one time__ once you've fixed __all__ the issues above, and I will re-review your bot. Thanks!")
+    msg1 = '\n'.join(msg)
+    pyperclip.copy(msg1)
+    print('The message has been copied to your clipboard!')
+elif start == '2':
+    botlist = ["If the bot is not in this list, don't send anything."]
+    ownerlist = ['Placeholder']
+    num = 1
+    with open(datafile) as json_file:
+        data = json.load(json_file)
+    for x in data['bots']:
+        bot = x['bot']
+        botlist.append(f"{num}. {bot}")
+        ownerlist.append(x['owner'])
         num += 1
-msg.append("Please ping me __one time__ once you've fixed __all__ the issues above, and I will re-review your bot. Thanks!")
-msg1 = '\n'.join(msg)
-pyperclip.copy(msg1)
-print('The message has been copied to your clipboard!')
+    botlist.append("Please select the number that corresponds to the bot you want to re-review: ")
+    bot_num = input('\n'.join(botlist))
+    if bot_num == '':
+        owner_mention = ''
+        bot_id = None
+    else:
+        owner_id = ownerlist.pop(int(bot_num))
+        owner_mention = f'<@{owner_id}>'
+    a = 0
+    num = 1
+    msg =[f'Hello {owner_mention}! After re-reviewing your bot, I found the following issues:']
+    print('Start typing errors below!')
+    while a == 0:
+        issue = input('\n- ')
+        if issue == '':
+            a = 1
+        else:
+            if issue.endswith('.') == False:
+                issue = f'{issue}.'
+            msg.append(f"> {num}. {issue}")
+            num += 1
+    msg.append("Please ping me __one time__ once you've fixed __all__ the issues above, and I will re-review your bot. Thanks!")
+    msg1 = '\n'.join(msg)
+    pyperclip.copy(msg1)
+    print('The message has been copied to your clipboard!')
+elif start == '3':
+    botlist = []
+    ownerlist = ['Placeholder']
+    delbotlist = ['Placeholder']
+    num = 1
+    with open(datafile) as json_file:
+        data = json.load(json_file)
+    for x in data['bots']:
+        bot = x['bot']
+        botlist.append(f"{num}. {bot}")
+        delbotlist.append(bot)
+        ownerlist.append(x['owner'])
+        num += 1
+    botlist.append("Please select the number that corresponds to the bot you want to remove: ")
+    del_list = input('\n'.join(botlist))
+    ownerid = ownerlist.pop(int(del_list))
+    botuser = delbotlist.pop(int(del_list))
+    for b in data['bots']:
+        if ownerid == b['owner'] and botuser == b['bot']:
+            data['bots'].remove(b)
+    with open(datafile, 'w') as f:
+        json.dump(data, f, indent=4)
+    print('Successfully Removed the Bots from the List!')
+else:
+    print('Nope! Try again.')
